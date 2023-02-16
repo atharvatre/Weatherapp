30eb36f34ee9f5c40c4fb66546effc4627c46ce3
Atharv Atre <91801614+atharvatre@users.noreply.github.com>
2023-02-17 03:39:37 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '117db6d3f3mshd0534a9abac0d19p179f69jsne071b5ee9b74',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY="0fc8ee1ac28902ce2225f84e12ac620b"
